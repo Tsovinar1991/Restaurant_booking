a3cdf931db56cb9aef6d4d3e0a7f4cd9e33eb680
Tsovinar <tsovinar.nemesida.grigoryan@gmail.com>
2019-01-31 18:02:49 +0400

    updated

---
+++ database/migrations/2019_01_31_073736_create_restaurant_menus_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('avatar');
+            $table->integer('parent_id')->default(0)->nullable();
+            $table->integer('restaurant_id')->unsigned();
+            $table->integer('price');
+            $table->string('weight');
+            $table->integer('status')->default(1);
+
+
+            $table->timestamps();
+
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant_menus');
+    }
+}
