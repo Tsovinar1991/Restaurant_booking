ceee6f6a68aa0d3bb1f686d068142c3ca1a5a0a9
Tsovinar <tsovinar.nemesida.grigoryan@gmail.com>
2019-02-28 13:24:33 +0400

    change

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\AdminRoles;
+use App\Role;
 
 
 class AdminUserController extends Controller
 {
     public function registerForm(){
 
-        $roles = AdminRoles::select('id', 'name')->get();
+        $roles = Role::select('id', 'name')->get();
         if(count($roles)> 0 ) {
             return view('admin.users.userRegisterForm', compact('roles'));
         }
