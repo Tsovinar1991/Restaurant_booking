38d3ac38059e2cfcd37029cf68a5d380e5e3c4e5
Tsovinar <tsovinar.nemesida.grigoryan@gmail.com>
2019-03-12 17:52:44 +0400

    fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,6 +4,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class Cors
 {
@@ -25,7 +26,7 @@ class Cors
         ];
         if ($request->getMethod() == "OPTIONS") {
 // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return \Response::make('OK', 200, $headers);
+            return Response::make('OK', 200, $headers);
         }
         $response = $next($request);
         foreach ($headers as $key => $value)
